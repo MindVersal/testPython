4da527f3c0112829d1984d94ad2bc93557a80048
MindVersal <antajchik@gmail.com>
2017-05-18 09:11:14 +0300

    09:11 18.05.2017

--- Chapter04.py
+++ Chapter04.py
@@ -3,5 +3,6 @@ import turtle
 print("Chapter 04.")
 print("Turtle")
 t = turtle.Pen()
+t.forward(50)
 
 print("THE END.")
---
+++ Chapter06.py
@@ -0,0 +1,5 @@
+print("Chapter 06.")
+for x in range(-10, 10):
+    print("Test %s" % x)
+
+print("THE END.")
