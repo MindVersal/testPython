116602a720dc8701360cf61ecadaad3ee5420c90
MindVersal <antajchik@gmail.com>
2017-05-18 09:13:32 +0300

    09:15 18.05.2017

--- Chapter06.py
+++ Chapter06.py
@@ -1,5 +1,6 @@
-print("Chapter 06.")
+print("Chapter 06.\n")
 for x in range(-10, 10):
     print("Test %s" % x)
-
-print("THE END.")
+print("\nList Range:")
+print(list(range(0, 10)))
+print("\nTHE END.")
