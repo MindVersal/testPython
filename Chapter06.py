b685309af982c044926e496d4dbce13ce9a65860
MindVersal <antajchik@gmail.com>
2017-05-18 13:42:31 +0300

    13:43 18.05.2017

--- Chapter06.py
+++ Chapter06.py
@@ -3,4 +3,10 @@ for x in range(-10, 10):
     print("Test %s" % x)
 print("\nList Range:")
 print(list(range(0, 10)))
+print("\nInput number")
+number = input("Number is: ")
+if bool(number.strip()):
+    print("Your answer is %s" % number)
+elif not bool(number.strip()):
+    print("Sorry, but your answer is wrong...")
 print("\nTHE END.")
