c6e57c3da2804e32c5574dde51e1e1f17ff63ba1
MindVersal <antajchik@gmail.com>
2017-05-18 13:58:07 +0300

    13:57 18.05.2017

--- Chapter06.py
+++ Chapter06.py
@@ -9,4 +9,11 @@ if bool(number.strip()):
     print("Your answer is %s" % number)
 elif not bool(number.strip()):
     print("Sorry, but your answer is wrong...")
+print('\nTest File:')
+test_file = open('./test.txt')
+text = test_file.read()
+print(text)
+print('\nTest sum')
+test_a = abs(10) + -10
+print(test_a)
 print("\nTHE END.")
