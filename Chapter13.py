0b04f17d76a8cfc7b1e01144f108e1f2c8394e1c
MindVersal <antajchik@gmail.com>
2017-05-19 13:18:09 +0300

    13:17 19.05.2017

--- Chapter13.py
+++ Chapter13.py
@@ -5,6 +5,15 @@ import time
 print('Chapter 13.')
 print('Game')
 
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+    def draw(self):
+        pass
+
+
 tk = Tk()
 tk.title("My Games")
 tk.resizable(0, 0)
@@ -12,5 +21,10 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
+ball = Ball(canvas, 'red')
+while True:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
 
 print('THE END.')
