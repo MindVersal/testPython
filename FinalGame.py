7cfb21ad0b90dd64c63013203e917acc586f9cca
MindVersal <antajchik@gmail.com>
2017-05-23 11:50:45 +0300

    11:50 22.05.2017

--- FinalGame.py
+++ FinalGame.py
@@ -124,7 +124,7 @@ class StickFigureSprite(Sprite):
             PhotoImage(file="./img/Figure-R3.gif"),
             PhotoImage(file="./img/Figure-R4.gif")
         ]
-        self.image = game.canvas.create_image(200, 470, image=self.images_left[0], anchor='nw')
+        self.image = game.canvas.create_image(200, 420, image=self.images_left[0], anchor='nw')
         self.x = -2
         self.y = 0
         self.current_image = 0
@@ -169,9 +169,73 @@ class StickFigureSprite(Sprite):
             else:
                 self.game.canvas.itemconfig(self.image, image=self.images_right[self.current_image])
 
+    def coords(self):
+        xy = self.game.canvas.coords(self.image)
+        self.coordinates.x1 = xy[0]
+        self.coordinates.y1 = xy[1]
+        self.coordinates.x2 = xy[0] + 35
+        self.coordinates.y2 = xy[1] + 45
+        return self.coordinates
+
+    def move(self):
+        self.animate()
+        if self.y < 0:
+            self.jump_count += 1
+            if self.jump_count > 30:
+                self.y = 4
+        if self.y > 0:
+            self.jump_count -= 1
+        co = self.coords()
+        left = True
+        right = True
+        top = True
+        bottom = True
+        falling = True
+        if (self.y > 0) and (co.y2 >= self.game.canvas_height):
+            self.y = 0
+            bottom = False
+        elif (self.y < 0) and (co.y1 <= 0):
+            self.y = 0
+            top = False
+        if (self.x > 0) and (co.x2 >= self.game.canvas_width):
+            self.x = 0
+            right = False
+        elif (self.x < 0) and (co.x1 <= 0):
+            self.x = 0
+            left = False
+        for sprite in self.game.sprites:
+            if sprite == self:
+                continue
+            sprite_co = sprite.coords()
+            if top and (self.y < 0) and sprite_co.collided_top(co, sprite_co):
+                self.y = -self.y
+                top = False
+            if bottom and (self.y > 0) and sprite_co.collided_bottom(self.y, co, sprite_co):
+                self.y = sprite_co.y1 - co.y2
+                if self.y < 0:
+                    self.y = 0
+                bottom = False
+                top = False
+            if bottom and falling and (self.y == 0) \
+                    and (co.y2 < self.game.canvas_height) \
+                    and sprite_co.collided_bottom(1, co, sprite_co):
+                falling = False
+            if left and (self.x < 0) and sprite_co.collided_left(co, sprite_co):
+                self.x = 0
+                left = False
+            if right and (self.x > 0) and sprite_co.collided_right(co, sprite_co):
+                self.x = 0
+                right = False
+        if falling and bottom and (self.y == 0) \
+                and (co.y2 < self.game.canvas_height):
+            self.y = 4
+        self.game.canvas.move(self.image, self.x, self.y)
+
 g = Game()
-platform1 = PlatformSprite(g, PhotoImage(file="./img/Platform1.gif"), 100, 280, 32, 10)
-platform2 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 50, 380, 66, 10)
+platform1 = PlatformSprite(g, PhotoImage(file="./img/Platform1.gif"), 200, 280, 32, 10)
+platform2 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 100, 400, 66, 10)
 platform3 = PlatformSprite(g, PhotoImage(file="./img/Platform3.gif"), 0, 480, 100, 10)
 g.sprites.append(platform1)
+sf = StickFigureSprite(g)
+g.sprites.append(sf)
 g.mainloop()
