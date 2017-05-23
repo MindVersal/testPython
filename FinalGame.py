e79f1d910e18c8a0c914f02046d72e8cfc3ae145
MindVersal <antajchik@gmail.com>
2017-05-23 12:53:44 +0300

    12:55 22.05.2017

--- FinalGame.py
+++ FinalGame.py
@@ -76,16 +76,14 @@ class Coords:
         if self.within_x(co1, co2):
             if (co1.y1 <= co2.y2) and (co1.y1 >= co2.y1):
                 return True
-        else:
-            return False
+        return False
 
     def collided_bottom(self, y, co1, co2):
         if self.within_x(co1, co2):
             y_calc = co2.y2 + y
             if (y_calc >= co2.y1) and (y_calc <= co2.y2):
                 return True
-        else:
-            return False
+        return False
 
 
 class Sprite:
@@ -124,11 +122,11 @@ class StickFigureSprite(Sprite):
             PhotoImage(file="./img/Figure-R3.gif"),
             PhotoImage(file="./img/Figure-R4.gif")
         ]
-        self.image = game.canvas.create_image(200, 420, image=self.images_left[0], anchor='nw')
+        self.image = game.canvas.create_image(200, 420, image=self.images_left[1], anchor='nw')
         self.x = -2
         self.y = 0
         self.current_image = 0
-        self.current_image_add = 0
+        self.current_image_add = 1
         self.jump_count = 0
         self.last_time = time.time()
         self.coordinates = Coords()
@@ -244,12 +242,27 @@ class DoorSprite(Sprite):
         self.coordinates = Coords(x, y, x + (width / 2.0), y + height)
         self.endgame = True
 
-
-
 g = Game()
-platform1 = PlatformSprite(g, PhotoImage(file="./img/Platform1.gif"), 200, 280, 32, 10)
-platform2 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 100, 400, 66, 10)
-platform3 = PlatformSprite(g, PhotoImage(file="./img/Platform3.gif"), 0, 480, 100, 10)
+platform1 = PlatformSprite(g, PhotoImage(file="./img/Platform3.gif"), 0, 480, 100, 10)
+platform2 = PlatformSprite(g, PhotoImage(file="./img/Platform3.gif"), 150, 440, 100, 10)
+platform3 = PlatformSprite(g, PhotoImage(file="./img/Platform3.gif"), 300, 400, 100, 10)
+platform4 = PlatformSprite(g, PhotoImage(file="./img/Platform3.gif"), 300, 160, 100, 10)
+platform5 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 175, 350, 66, 10)
+platform6 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 50, 300, 66, 10)
+platform7 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 170, 120, 66, 10)
+platform8 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 45, 60, 66, 10)
+platform9 = PlatformSprite(g, PhotoImage(file="./img/Platform1.gif"), 170, 250, 32, 10)
+platform10 = PlatformSprite(g, PhotoImage(file="./img/Platform1.gif"), 230, 200, 32, 10)
+g.sprites.append(platform1)
+g.sprites.append(platform2)
+g.sprites.append(platform3)
+g.sprites.append(platform4)
+g.sprites.append(platform5)
+g.sprites.append(platform6)
+g.sprites.append(platform7)
+g.sprites.append(platform8)
+g.sprites.append(platform9)
+g.sprites.append(platform10)
 g.sprites.append(platform1)
 door = DoorSprite(g, PhotoImage(file="./img/Door1.gif"), 45, 30, 40, 35)
 g.sprites.append(door)
