47453e9845d3257652d7255ba908a3c2a5c90891
MindVersal <antajchik@gmail.com>
2017-05-23 10:05:48 +0300

    10:10 22.05.2017

--- FinalGame.py
+++ FinalGame.py
@@ -109,6 +109,44 @@ class PlatformSprite(Sprite):
         self.coordinates = Coords(x, y, x + width, y + height)
 
 
+class StickFigureSprite(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self, game)
+        self.images_left = [
+            PhotoImage(file="./img/Figure-L1.gif"),
+            PhotoImage(file="./img/Figure-L2.gif"),
+            PhotoImage(file="./img/Figure-L3.gif")
+        ]
+        self.images_right = [
+            PhotoImage(file="./img/Figure-R1.gif"),
+            PhotoImage(file="./img/Figure-R2.gif"),
+            PhotoImage(file="./img/Figure-R3.gif")
+        ]
+        self.image = game.canvas.create_image(200, 470, image=self.images_left[0], anchor='nw')
+        self.x = -2
+        self.y = 0
+        self.current_image = 0
+        self.current_image_add = 0
+        self.jump_count = 0
+        self.last_time = time.time()
+        self.coordinates = Coords()
+        game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        game.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        game.canvas.bind_all('<space>', self.jump)
+
+    def turn_left(self, evt):
+        if self.y == 0:
+            self.x = -2
+
+    def turn_right(self, evt):
+        if self.y == 0:
+            self.x = 2
+
+    def jump(self, evt):
+        if self.y == 0:
+            self.y = -4
+            self.jump_count = 0
+
 g = Game()
 platform1 = PlatformSprite(g, PhotoImage(file="./img/Platform1.gif"), 100, 280, 32, 10)
 platform2 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 50, 380, 66, 10)
