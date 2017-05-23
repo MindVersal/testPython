d39392cc516eba254cc22f6fd9ac3d064d26bff0
MindVersal <antajchik@gmail.com>
2017-05-23 08:50:49 +0300

    08:50 22.05.2017

--- FinalGame.py
+++ FinalGame.py
@@ -88,5 +88,30 @@ class Coords:
             return False
 
 
+class Sprite:
+    def __init__(self, game):
+        self.game = game
+        self.endgame = False
+        self.coordinates = None
+
+    def move(self):
+        pass
+
+    def coords(self):
+        return self.coordinates
+
+
+class PlatformSprite(Sprite):
+    def __init__(self, game, photo_image, x, y, width, height):
+        Sprite.__init__(self, game)
+        self.photo_image = photo_image
+        self.image = game.canvas.create_image(x, y, image=self.photo_image, anchor='nw')
+        self.coordinates = Coords(x, y, x + width, y + height)
+
+
 g = Game()
+platform1 = PlatformSprite(g, PhotoImage(file="./img/Platform1.gif"), 100, 280, 32, 10)
+platform2 = PlatformSprite(g, PhotoImage(file="./img/Platform2.gif"), 50, 380, 66, 10)
+platform3 = PlatformSprite(g, PhotoImage(file="./img/Platform3.gif"), 0, 480, 100, 10)
+g.sprites.append(platform1)
 g.mainloop()
