f33e9ccc80ba91544f8c2f7bf09d3d189146e39b
MindVersal <antajchik@gmail.com>
2017-05-24 16:29:55 +0300

    16:30 24.05.2017

--- PassGen.py
+++ PassGen.py
@@ -4,10 +4,10 @@ import random
 def pass_gen(count_char=8):
     dict_values = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                   's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0',
+                   's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.',
                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
                    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.',
+                   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0',
                    '1', '2', '3', '4', '5', '6', '7', '8', '9']
     passw = []
     for i in range(count_char):
