86536f78d5ab0771cf457ba2408e6d0705b3ab38
MindVersal <antajchik@gmail.com>
2017-05-31 14:25:50 +0300

    14:25 31.05.2017

--- testGismeteoTemp.py
+++ testGismeteoTemp.py
@@ -6,4 +6,4 @@ res.raise_for_status()
 no_start_soup = bs4.BeautifulSoup(res.text, "html.parser")
 elems = no_start_soup.select('#middle dt a')
 for elem in elems:
-    print(elem)
+    print(elem.getText())
