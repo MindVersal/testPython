e1b10d3fd275187cc692525325e274e2faa0d92d
MindVersal <antajchik@gmail.com>
2017-06-01 17:17:36 +0300

    17:17 01.06.2017

---
+++ testNOC.py
@@ -0,0 +1,17 @@
+from grab import Grab
+
+print('Test NOC DB')
+url_name_login = 'http://10.166.0.27/db/index.php'
+url_name_report = 'http://10.166.0.27/db/index.php?a=repAdmin&c=sendForm&r=equipInventory&id_pop=3533&id_equip_type=&id_port_type='
+g = Grab(timeout=100, connect_timeout=20)
+g.go(url_name_login)
+g.doc.set_input('login', 'pia@ex.tchercom.ru')
+print('Insert password:')
+password = input()
+g.doc.set_input('password', password)
+g.doc.submit()
+resp = g.go(url_name_report)
+resp.charset = 'cp1251'
+open('./temp_report.html', 'w').write(resp.unicode_body())
+
+print('\n\nTHE END.')
---
+++ testNOCtext.py
@@ -0,0 +1,46 @@
+from grab import Grab
+
+print('Grab text from temp_report.html')
+g = Grab()
+url_name = 'file://temp_report.html'
+resp = g.go(url_name, charset='cp1251')
+print('\nИтого портов:')
+print('GPON:')
+print('Монтировано: ', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?GPON\s+(.+\n){2}.+\(([^<]+)\)\s+').group(3))
+print('Активно: ', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?GPON\s+(.+\n){4}.+\(([^<]+)\)\s+').group(3))
+print('Задействовано: ', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?GPON\s+(.+\n){6}.+\(([^<]+)\)\s+').group(3))
+print('Свободно: ', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?GPON\s+(.+\n){7}.+\s+([^<]+)\s+').group(3))
+print('\nADSL:')
+print('Монтировано:', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?ADSL2\+\s+(.+\n){1}.+\s+([^<]+)\s+').group(3))
+print('Активно: ', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?ADSL2\+\s+(.+\n){3}.+\s+([^<]+)\s+').group(3))
+print('Задействовано: ', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?ADSL2\+\s+(.+\n){5}.+\s+([^<]+)\s+').group(3))
+print('Свободно: ', end='')
+print(g.doc.rex_search(r'Итого(.|\n|\r|\s)*?ADSL2\+\s+(.+\n){7}.+\s+([^<]+)\s+').group(3))
+print('\nПортов на 1T3:', end='')
+name_ats = ['31', '26']
+for ats in name_ats:
+    print('\nАТС ' + str(ats) + ':')
+    print('Монтировано:', end='')
+    print(g.doc.rex_search(r'8202' + str(ats) + r'(.|\n|\r|\s)*?1T3(.|\n|\r|\s)*?GPON(.|\n|\r|\s)*?' +
+                           r'summary(.*\n*){2}\s+\d+\(([^<]+)\)\s+').group(5))
+    print('Активно: ', end='')
+    print(g.doc.rex_search(r'8202' + str(ats) + r'(.|\n|\r|\s)*?1T3(.|\n|\r|\s)*?GPON(.|\n|\r|\s)*?' +
+                           r'summary(.*\n*){4}\s+\d+\(([^<]+)\)\s+').group(5))
+    print('Задействовано: ', end='')
+    print(g.doc.rex_search(r'8202' + str(ats) + r'(.|\n|\r|\s)*?1T3(.|\n|\r|\s)*?GPON(.|\n|\r|\s)*?' +
+                           r'summary(.*\n*){6}\s+\d+\(([^<]+)\)\s+').group(5))
+    print('Свободно: ', end='')
+    print(g.doc.rex_search(r'8202' + str(ats) + r'(.|\n|\r|\s)*?1T3(.|\n|\r|\s)*?GPON(.|\n|\r|\s)*?' +
+                           r'summary(.*\n*){8}\s+(\d+)([^<]+)\s+').group(5))
+
+print('\nTest:')
+
+
+print('\n\nTHE END.')
