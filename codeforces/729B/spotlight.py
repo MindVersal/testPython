8e3d8934d6a7eef3052fd70b62c48e1cc04b2af6
MindVersal <antajchik@gmail.com>
2017-06-13 15:26:00 +0300

    15:25 03.06.2017

--- codeforces/729B/spotlight.py
+++ codeforces/729B/spotlight.py
@@ -75,6 +75,78 @@ B. Прожекторы
 
 Таким образом, в данном примере всего 9 хороших позиций.
 """
+import copy
+
+
+def print_arrary(temp_array):
+    for row in range(len(temp_array)):
+        print(temp_array[row])
+
+
+def count_neighbors_left_to_right(temp_array, count_rows, count_cols,  row, col):
+    result = 0
+    temp_offset = 1
+    while (col - temp_offset) >= 0:
+        if temp_array[row][col - temp_offset] == 1:  # West
+            result += 1
+            temp_array[row][col] = 1
+            break
+        elif temp_array[row][col - temp_offset] == -1:
+            break
+        temp_offset += 1
+    temp_offset = 1
+    while (col + temp_offset) < count_cols:
+        if temp_array[row][col + temp_offset] == 1:  # East
+            result += 1
+            break
+        elif temp_array[row][col + temp_offset] == -1:
+            break
+        temp_offset += 1
+    if result == 0:
+        temp_array[row][col] = -1
+    elif result == 2:
+        temp_array[row][col] = 1
+    return result
+
+
+def count_neighbors_top_to_bottom(temp_array, count_rows, count_cols, row, col):
+    result = 0
+    temp_offset = 1
+    while (row - temp_offset) >= 0:
+        if temp_array[row - temp_offset][col] == 1:  # West
+            result += 1
+            break
+        elif temp_array[row - temp_offset][col] == -1:
+            break
+        temp_offset += 1
+    temp_offset = 1
+    while (row + temp_offset) < count_rows:
+        if temp_array[row + temp_offset][col] == 1:  # East
+            result += 1
+            break
+        elif temp_array[row + temp_offset][col] == -1:
+            break
+        temp_offset += 1
+    if result == 0:
+        temp_array[row][col] = -1
+    elif result == 2:
+        temp_array[row][col] = 1
+    return result
+
+
+def count_best_positions_two_steps(count_rows, count_cols):
+    result = 0
+    temp_array = copy.deepcopy(input_array)
+    for i in range(count_rows):
+        for j in range(count_cols):
+            if temp_array[i][j] != 1:
+                result += count_neighbors_left_to_right(temp_array, count_rows, count_cols, i, j)
+    temp_array = copy.deepcopy(input_array)
+    for j in range(count_cols):
+        for i in range(count_rows):
+            if temp_array[i][j] != 1:
+                result += count_neighbors_top_to_bottom(temp_array, count_rows, count_cols, i, j)
+    return result
 
 
 def count_neighbors(row, col):
@@ -92,6 +164,8 @@ def count_neighbors(row, col):
             break
         temp_offset += 1
     temp_offset = 1
+    if result == 2:
+        input_array[row][col] = 1
     while (row + temp_offset) < len(input_array):
         if input_array[row + temp_offset][col] == 1:  # South
             result += 1
@@ -117,21 +191,29 @@ def count_best_positions():
 
 input_array = []
 
-# file = open('./input.txt')
-# temp_array_string = file.readline().split()
-temp_array_string = input().split()
+file = open('./input.txt')
+temp_array_string = file.readline().split()
+# temp_array_string = input().split()
 count_rows = int(temp_array_string[0])  # i == n
 count_cols = int(temp_array_string[1])  # j == m
 for row in range(count_rows):
-    # temp_array_string = [int(cell) for cell in file.readline().split()]
-    temp_array_string = [int(cell) for cell in input().split()]
+    temp_array_string = [int(cell) for cell in file.readline().split()]
+    # temp_array_string = [int(cell) for cell in input().split()]
     temp_row = []
     for col in range(count_cols):
         temp_row.append(temp_array_string[col])
     input_array.append(temp_row)
+print(count_best_positions_two_steps(count_rows, count_cols))
+input_array = []
+count_rows = 100
+count_cols = 400
+for i in range(count_rows):
+    input_array.append([])
+    for j in range(count_cols):
+        input_array[i].append(0)
+input_array[0][0] = 1
+print(count_best_positions_two_steps(count_rows, count_cols))
 
-print(count_best_positions())
-
-# file.close()
+file.close()
 
 print()
