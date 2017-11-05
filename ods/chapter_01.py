2f379ca91f37ef8e47ec9163a04af030a7a77ac1
MindVersal <antajchik@gmail.com>
2017-11-05 12:53:25 +0300

    12:51 05.11.2017

--- ods/chapter_01.py
+++ ods/chapter_01.py
@@ -2,12 +2,26 @@ import numpy as np
 import pandas as pd
 
 
+def detect_gender(pd):
+    gender_1 = df[df['gender'] == 1]
+    gender_2 = df[df['gender'] == 2]
+    gender_1_mean_height = gender_1['height'].mean()
+    gender_2_mean_height = gender_2['height'].mean()
+    if gender_1_mean_height > gender_2_mean_height:
+        gender_1_name = 'man'
+        gender_2_name = 'women'
+    else:
+        gender_1_name = 'women'
+        gender_2_name = 'men'
+    print('Count ({}) gender == 1 : {}, mean height : {}'.
+          format(gender_1_name, len(gender_1), gender_1_mean_height))
+    print('Count ({}) gender == 2 : {}, mean height : {}'.
+          format(gender_2_name, len(gender_2), gender_2_mean_height))
+
+
 if __name__ == '__main__':
     df = pd.read_csv('../data/mlbootcamp5_train.csv', sep=';', index_col='id')
     pd.set_option('display.width', 100)
     print('DataFrame head: \n{}'.format(df.head()))
     print('DataFrame shape: {}'.format(df.shape))
-    print('Count (women) gender == 1 : {}, mean height : {}'.
-          format(len(df[df['gender'] == 1]), df[df['gender'] == 1]['height'].mean()))
-    print('Count ( men ) gender == 2 : {}, mean height : {}'.
-          format(len(df[df['gender'] == 2]), df[df['gender'] == 2]['height'].mean()))
+    detect_gender(pd)
