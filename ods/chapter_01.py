10fa847fafbacc966d1096c9e56a7b2ff6a89758
MindVersal <antajchik@gmail.com>
2017-11-05 13:21:13 +0300

    13:23 05.11.2017

--- ods/chapter_01.py
+++ ods/chapter_01.py
@@ -2,21 +2,30 @@ import numpy as np
 import pandas as pd
 
 
-def detect_gender(pd):
+def detect_gender(df):
     gender_1 = df[df['gender'] == 1]
     gender_2 = df[df['gender'] == 2]
     gender_1_mean_height = gender_1['height'].mean()
     gender_2_mean_height = gender_2['height'].mean()
     if gender_1_mean_height > gender_2_mean_height:
-        gender_1_name = 'man'
+        gender_1_name = 'men'
         gender_2_name = 'women'
     else:
         gender_1_name = 'women'
         gender_2_name = 'men'
-    print('Count ({}) gender == 1 : {}, mean height : {}'.
-          format(gender_1_name, len(gender_1), gender_1_mean_height))
-    print('Count ({}) gender == 2 : {}, mean height : {}'.
-          format(gender_2_name, len(gender_2), gender_2_mean_height))
+    print('{} {} and {} {}'.
+          format(len(gender_1), gender_1_name, len(gender_2), gender_2_name))
+
+
+def detect_alcohol(pd):
+    gender_1_name = 'women'
+    gender_2_name = 'man'
+    count_alcohol_gender_1 = df[df['gender'] == 1]['alco'].mean()
+    count_alcohol_gender_2 = df[df['gender'] == 2]['alco'].mean()
+    if count_alcohol_gender_1 > count_alcohol_gender_2:
+        print('women')
+    else:
+        print('men')
 
 
 if __name__ == '__main__':
@@ -24,4 +33,7 @@ if __name__ == '__main__':
     pd.set_option('display.width', 100)
     print('DataFrame head: \n{}'.format(df.head()))
     print('DataFrame shape: {}'.format(df.shape))
-    detect_gender(pd)
+    print('How much women and men in DataFrame:')
+    detect_gender(df)
+    print('Who in mean write, what have alcohol: ')
+    detect_alcohol(df)
