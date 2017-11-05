52c6e56bb94942c81ea19ddbe0bd8723782a19de
MindVersal <antajchik@gmail.com>
2017-11-05 12:21:40 +0300

    12:21 05.11.2017

--- ods/chapter_01.py
+++ ods/chapter_01.py
@@ -5,5 +5,5 @@ import pandas as pd
 if __name__ == '__main__':
     df = pd.read_csv('../data/mlbootcamp5_train.csv', sep=';', index_col='id')
     pd.set_option('display.width', 100)
-    print('DataFrame head: \n')
-    print(df.head())
+    print('DataFrame head: \n{}'.format(df.head()))
+    print('DataFrame shape: {}'.format(df.shape))
