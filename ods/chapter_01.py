9f4e99dcdbf81cf0f91acc8f359c11766c0df539
MindVersal <antajchik@gmail.com>
2017-11-05 12:04:14 +0300

    12:12 05.11.2017

---
+++ ods/chapter_01.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../data/mlbootcamp5_train.csv', sep=';', index_col='id')
+    pd.set_option('display.width', 100)
+    print('DataFrame head: \n')
+    print(df.head())
