e01d309ea78fed898058ec0ca1ac93e122925b91
MindVersal <antajchik@gmail.com>
2017-11-11 12:47:36 +0300

    12:47 11.11.2017

---
+++ ods/test_chapter_02.py
@@ -0,0 +1,12 @@
+from pylab import rcParams
+import pandas as pd
+import seaborn as sns
+
+
+def test_seaborn_and_ploty():
+    print('Test Seaborn and Ploty.')
+
+
+if __name__ == '__main__':
+    rcParams['figure.figsize'] = 8, 5
+    test_seaborn_and_ploty()
