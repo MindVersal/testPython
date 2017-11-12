2e158ed82a8041a90228d9af46f3f7abcc2b2ea5
MindVersal <antajchik@gmail.com>
2017-11-12 13:37:07 +0300

    13:31 12.11.2017

--- ods/test_chapter_02.py
+++ ods/test_chapter_02.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from pylab import rcParams
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
 
 def test_seaborn_and_ploty():
@@ -29,11 +30,59 @@ def test_seaborn_and_ploty():
     # 3
     # sns.distplot(df.Critic_Score)
     # 4
-    top_platforms = df.Platform.value_counts().sort_values(ascending=False).head(5).index.values
-    sns.boxplot(y='Platform', x='Critic_Score', data=df[df.Platform.isin(top_platforms)], orient='h')
+    # top_platforms = df.Platform.value_counts().sort_values(ascending=False).head(5).index.values
+    # sns.boxplot(y='Platform', x='Critic_Score', data=df[df.Platform.isin(top_platforms)], orient='h')
+    # 5
+    platform_genre_sales = df.pivot_table(index='Platform',
+                                          columns='Genre',
+                                          values='Global_Sales',
+                                          aggfunc=sum).fillna(0).applymap(float)
+    sns.heatmap(platform_genre_sales, annot=True, fmt='.1f', linewidths=.5)
+    plt.show()
+
+
+def test_telecom():
+    print('Test Telecom Churn')
+    df = pd.read_csv('../data/telecom_churn.csv')
+    pd.set_option('display.width', 120)
+    print(df.head())
+    print('Shape : {}'.format(df.shape))
+    # 1
+    # df['Churn'].value_counts().plot(kind='bar', label='Churn')
+    # 2
+    # corr_matrix = df.drop(['State', 'International plan', 'Voice mail plan', 'Area code'], axis=1).corr()
+    # sns.heatmap(corr_matrix)
+    # 3
+    features = list(set(df.columns) - set(['State', 'International plan', 'Voice mail plan', 'Area code',
+                                           'Total day charge', 'Total eve charge', 'Total night charge',
+                                           'Total intl charge', 'Churn']))
+    # df[features].hist(figsize=(20, 12))
+    # 4
+    # sns.pairplot(df[features + ['Churn']], hue='Churn')
+    # 5
+    # print(len(features))
+    # fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(16, 10))
+    # for idx, feat in enumerate(features):
+    #     sns.boxplot(x='Churn', y=feat, data=df, ax=axes[idx / 4, idx % 4])
+    #     axes[idx / 4, idx % 4].legend()
+    #     axes[idx / 4, idx % 4].set_xlabel('Churn')
+    #     axes[idx / 4, idx % 4].set_ylabel(feat)
+    # 6
+    # _, axes = plt.subplots(1, 2, sharey=True, figsize=(16, 6))
+    # sns.boxplot(x='Churn', y='Total day minutes', data=df, ax=axes[0])
+    # sns.violinplot(x='Churn', y='Total day minutes', data=df, ax=axes[1])
+    # 7
+    # sns.countplot(x='Customer service calls', hue='Churn', data=df)
+    # 8
+    # _, axes = plt.subplots(1, 2, sharey=True, figsize=(16, 6))
+    # sns.countplot(x='International plan', hue='Churn', data=df, ax=axes[0])
+    # sns.countplot(x='Voice mail plan', hue='Churn', data=df, ax=axes[1])
+    # 9
+    print(df.groupby(['State'])['Churn'].agg([np.mean]).sort_values(by='mean', ascending=False).head())
     plt.show()
 
 
 if __name__ == '__main__':
     rcParams['figure.figsize'] = 8, 5
-    test_seaborn_and_ploty()
+    # test_seaborn_and_ploty()
+    test_telecom()
