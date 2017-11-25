2eda71e7b5bd8d5e891adfc9d66e95a985398172
MindVersal <antajchik@gmail.com>
2017-11-25 11:49:57 +0300

    11:50 25.11.2017

---
+++ ods/hw_chapter_03.py
@@ -0,0 +1,20 @@
+import matplotlib.pylab as plt
+
+
+def init_first():
+    print('Homework for chapter 03')
+
+
+def test_adult():
+
+    plt.show()
+
+
+def last_end():
+    print('The End')
+
+
+if __name__ == '__main__':
+    init_first()
+
+    last_end()
