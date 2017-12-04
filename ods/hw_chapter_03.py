acf7be44bd0fcc7ad83ef62661204087739624fd
MindVersal <antajchik@gmail.com>
2017-12-04 10:00:42 +0300

    10:00 04.12.2017

--- ods/hw_chapter_03.py
+++ ods/hw_chapter_03.py
@@ -148,16 +148,16 @@ def analise_adult_dadaframe():
     data_test['Capital_Gain'] = data_test['Capital_Gain'].astype(np.int64)
     data_test['Capital_Loss'] = data_test['Capital_Loss'].astype(np.int64)
     data_test['Hours_per_week'] = data_test['Hours_per_week'].astype(np.int64)
-    print(data_test.dtypes)
+    # print(data_test.dtypes)
 
     categorical_columns_train = [c for c in data_train.columns if data_train[c].dtype.name == 'object']
     numerical_columns_train = [c for c in data_train.columns if data_train[c].dtype.name != 'object']
     categorical_columns_test = [c for c in data_test.columns if data_test[c].dtype.name == 'object']
     numerical_columns_test = [c for c in data_test.columns if data_test[c].dtype.name != 'object']
-    print('Categirical columns train: {}'.format(categorical_columns_train))
-    print('Categorical columns test:  {}'.format(categorical_columns_test))
-    print('Numerical columns train: {}'.format(numerical_columns_train))
-    print('Numerical columns test   {}'.format(numerical_columns_test))
+    # print('Categorical columns train: {}'.format(categorical_columns_train))
+    # print('Categorical columns test:  {}'.format(categorical_columns_test))
+    # print('Numerical columns train: {}'.format(numerical_columns_train))
+    # print('Numerical columns test   {}'.format(numerical_columns_test))
     for c in categorical_columns_train:
         data_train[c] = data_train[c].fillna(data_train[c].mode())
     for c in categorical_columns_test:
@@ -193,7 +193,15 @@ def analise_adult_dadaframe():
     data_test.drop(['Workclass', 'Education', 'Martial_Status', 'Occupation',
                     'Relationship', 'Race', 'Sex', 'Country'],
                    axis=1, inplace=True)
-    print(data_test.describe(include='all').T)
+    # print(data_test.describe(include='all').T)
+    # print(set(data_train.columns) - set(data_test.columns))
+    data_test['Country_ Holand-Netherlands'] = np.zeros([data_test.shape[0], 1])
+    # print(set(data_train.columns) - set(data_test.columns))
+    X_train = data_train.drop(['Target'], axis=1)
+    y_train = data_train['Target']
+    X_test = data_test.drop(['Target'], axis=1)
+    y_test = data_test['Target']
+
     plt.show()
 
 
