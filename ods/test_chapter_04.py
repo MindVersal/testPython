0d5e81e1011eff6cc1c4b4cfea6c526302fcaeec
MindVersal <antajchik@gmail.com>
2017-12-05 13:22:40 +0300

    13:22 05.12.2017

--- ods/test_chapter_04.py
+++ ods/test_chapter_04.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.model_selection import GridSearchCV
 
 
 def before_main():
@@ -14,11 +18,13 @@ def after_main():
 
 def testing_logres():
     print('Testing')
+    data = pd.read_csv('../data/microchip_tests.txt', header=None, names=('test1', 'test2', 'released'))
+    print(data.info())
 
-    plt.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
     before_main()
-
+    testing_logres()
     after_main()
