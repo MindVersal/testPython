67ee9da03490a017a92043abb758df362270740f
MindVersal <antajchik@gmail.com>
2017-12-05 12:52:48 +0300

    12:51 05.12.2017

---
+++ ods/test_chapter_04.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import seaborn
+import numpy as np
+import pandas as pd
+
+
+def before_main():
+    print('Testing Chapter 04.')
+
+
+def after_main():
+    print('\nThe End.')
+
+
+def testing_logres():
+    print('Testing')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    before_main()
+
+    after_main()
