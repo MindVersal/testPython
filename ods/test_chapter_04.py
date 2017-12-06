136a95335c3dbe681ede85dc97e3f8f9ff2ba88b
MindVersal <antajchik@gmail.com>
2017-12-06 14:09:57 +0300

    14:10 06.12.2017

--- ods/test_chapter_04.py
+++ ods/test_chapter_04.py
@@ -38,8 +38,8 @@ def plot_scatter_X(X, y):
 def testing_logres():
     print('Testing')
     data = pd.read_csv('../data/microchip_tests.txt', header=None, names=('test1', 'test2', 'released'))
-    print(data.info())
-    print(data.head())
+    # print(data.info())
+    # print(data.head())
     X = data.ix[:, :2].values
     y = data.ix[:, 2].values
     # plt.scatter(X[y == 1, 0], X[y == 1, 1], c='green', label='Ready')
@@ -57,9 +57,30 @@ def testing_logres():
     C = 1e+4
     logit = LogisticRegression(C=C, n_jobs=-1, random_state=17)
     logit.fit(X_poly, y)
-    plot_boundary(logit, X, y, grid_step=.01, poly_featurizer=poly)
-    plot_scatter_X(X, y)
+    # plot_boundary(logit, X, y, grid_step=.01, poly_featurizer=poly)
+    # plot_scatter_X(X, y)
     print('Round Score: {}'.format(round(logit.score(X_poly, y), 3)))
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=17)
+    c_values = np.logspace(-2, 3, 500)
+    logit_searcher = LogisticRegressionCV(Cs=c_values, cv=skf, verbose=1, n_jobs=-1)
+    logit_searcher.fit(X_poly, y)
+    print('Score: {}'.format(round(logit_searcher.score(X_poly, y), 3)))
+    # Print Validation
+    # i_min = 1
+    # i_max = 1000
+    # i_step = 1
+    # i_mult = 0.01
+    # plt.scatter([i * i_mult for i in range(i_min, i_max, i_step)],
+    #             [LogisticRegression(C=i*i_mult, n_jobs=-1, random_state=17).fit(X_poly, y).score(X_poly, y)
+    #              for i in range(i_min, i_max, i_step)], label='C')
+    # plt.xlabel('C')
+    # plt.ylabel('LogReg')
+    # plt.title('Test C')
+    # plt.legend()
+    # for c in [2, 10, 100, 300, 650, 990]:
+    #     logist_reg = LogisticRegression(C=c, n_jobs=-1, random_state=17).fit(X_poly, y)
+    #     print('Score LogisticRegression: {}'.format(logist_reg.score(X_poly, y)))
+
     plt.show()
 
 
